38d7e34fe106add7949866412bf4bf665d19f0ef
nodir asomov <nasomov93@gmail.com>
2026-08-24 15:49:19 +0500

    security

---
+++ infra/api-gateway/src/main/java/ru/yandex/practicum/gateway/security/GatewaySecurityProperties.java
@@ -0,0 +1,49 @@
+package ru.yandex.practicum.gateway.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigurationProperties(prefix = "app.security")
+public class GatewaySecurityProperties {
+    private List<User> users = new ArrayList<>();
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public static class User {
+        private String username;
+        private String password;
+        private List<String> roles = new ArrayList<>();
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public List<String> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(List<String> roles) {
+            this.roles = roles;
+        }
+    }
+}
